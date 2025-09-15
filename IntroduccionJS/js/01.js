90e5250315c5fde3f9ae370f927d82e24955e050
CMV\lofd394905 <david.lopez@cajamorelia.com.mx>
2025-09-15 15:30:00 -0600

    Update 01.js

---
+++ IntroduccionJS/js/01.js
@@ -0,0 +1,14 @@
+// variables con let
+
+let cliente = "David"
+let precioDescuento = 200
+
+// se puede reasignar
+cliente = "David L"
+cliente = true
+
+// se puede iniciar sin un valor
+let precio
+
+console.log(cliente)
+console.log(precio)
