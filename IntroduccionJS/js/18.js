f3d7a1c52ba11aebbbe26c776110e2b159d44263
CMV\lofd394905 <david.lopez@cajamorelia.com.mx>
2025-09-18 11:43:51 -0600

    19
    19

---
+++ IntroduccionJS/js/18.js
@@ -0,0 +1,12 @@
+// Operador de comparacion
+const numero1 = 20
+const numero2 = "20"
+
+if(numero1 === numero2) {
+    console.log('Si son iguales')
+} else {
+    console.log('no son iguales')
+}
+
+console.log(typeof numero1)
+console.log(typeof numero2)
