84b61106edb6a46ddbaa3a2a43e1ea92e9b44778
CMV\lofd394905 <david.lopez@cajamorelia.com.mx>
2025-09-22 18:01:26 -0600

    25
    25

---
+++ IntroduccionJS/js/23.js
@@ -0,0 +1,12 @@
+const heading = document.querySelector('.heading')
+
+console.log(heading)
+
+heading.textContent = 'Nuevo Heading'
+console.log(heading.textContent)
+console.log(heading.tagName)
+console.log(heading.classList)
+console.log(heading.id)
+
+const enlaces = document.querySelectorAll('.navegacion a')
+console.log(enlaces)
