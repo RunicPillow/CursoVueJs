a4396bb41a3406c0e25d909a3cb9abe40b313aa6
RunicPillow <javatar20@gmail.com>
2025-09-27 21:29:37 -0600

    26
    26

---
+++ IntroduccionJS/js/26.js
@@ -0,0 +1,17 @@
+const inputNombre = document.querySelector('#nombre')
+const inputPassword = document.querySelector('#password')
+const heading = document.querySelector('.heading')
+
+inputNombre.addEventListener('input', (e) => {
+    console.log(e.target.value)
+    console.log(e.target.value.length)
+    heading.textContent = e.target.value
+})
+
+inputPassword.addEventListener('input', () => {
+    inputPassword.type = 'text'
+    
+    setTimeout(() => {
+         inputPassword.type = 'password'
+    }, 500);
+})
