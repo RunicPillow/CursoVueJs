37c4b9ef058148e5f0ee52388064e5f55c1e8850
RunicPillow <javatar20@gmail.com>
2025-10-03 21:47:43 -0600

    29
    29

---
+++ IntroduccionJS/js/funciones.js
@@ -0,0 +1,16 @@
+export function sumar(numero1, numero2) {
+    return numero1 + numero2
+}
+
+export function restar(numero1, numero2) {
+    return numero1 - numero2
+}
+
+const hola = "Hola Mundo"
+
+export default hola
+
+// export {
+//     sumar,
+//     restar
+// } 
