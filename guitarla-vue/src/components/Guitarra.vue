79715758607f8528de39ea47712e64dc836b2d58
RunicPillow <javatar20@gmail.com>
2025-10-09 22:18:57 -0600

    52
    52

--- guitarla-vue/src/components/Guitarra.vue
+++ guitarla-vue/src/components/Guitarra.vue
@@ -1,4 +1,8 @@
 <script setup>
+    import { ref } from 'vue'
+
+    const numero = ref(0)
+
     const props = defineProps({
         guitarra: {
             type: Object,
@@ -6,6 +10,7 @@
         }
     })
 
+    
 </script>
 
 <template>
@@ -19,11 +24,13 @@
     </div>
     <div class="col-8">
         <h3 class="text-black fs-4 fw-bold text-uppercase">{{ guitarra.nombre }}</h3>
+        <p>{{ numero }}</p>
         <p> {{ guitarra.descripcion }} </p>
         <p class="fw-black text-primary fs-3"> {{ guitarra.precio }} </p>
         <button 
             type="button"
             class="btn btn-dark w-100 "
+            @click="incrementar"
         >Agregar al Carrito</button>
     </div>
 </div><!-- FIN GUITARRA -->
