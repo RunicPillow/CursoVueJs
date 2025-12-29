3daa4cc8a772a565a279c761c6a6e69f7d92fe3b
CMV\lofd394905 <david.lopez@cajamorelia.com.mx>
2025-12-29 16:11:29 -0600

    162
    162

--- cripto-vue/src/App.vue
+++ cripto-vue/src/App.vue
@@ -1,16 +1,12 @@
 <script setup>
-import { ref, reactive, onMounted, computed } from "vue";
+import { ref, reactive } from "vue";
 import Alerta from "./components/Alerta.vue";
 import Spinner from "./components/Spinner.vue";
+import useCripto from "./composables/useCripto";
 
-const monedas = ref([
-  { codigo: "USD", texto: "Dolar de Estados Unidos" },
-  { codigo: "MXN", texto: "Peso Mexicano" },
-  { codigo: "EUR", texto: "Euro" },
-  { codigo: "GBP", texto: "Libra Esterlina" },
-]);
+const { monedas, criptomonedas, cargando, cotizacion, obtenerCotizacion, mostrarResultado } = useCripto() // composable
 
-const criptomonedas = ref([]);
+
 const error = ref("");
 
 const cotizar = reactive({
@@ -18,18 +14,7 @@ const cotizar = reactive({
   criptomoneda: "",
 });
 
-const cotizacion = ref({})
-const cargando = ref(false)
 
-onMounted(() => {
-  const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD";
-  fetch(url)
-    .then((respuesta) => respuesta.json())
-    // .then(({Data}) => {
-    //   criptomonedas.value = Data
-    // })
-    .then(({ Data }) => (criptomonedas.value = Data));
-});
 
 const cotizarCripto = () => {
   // Validar que cotizar este lleno
@@ -39,32 +24,11 @@ const cotizarCripto = () => {
   }
 
   error.value = "";
-  obtenerCotizacion();
+  obtenerCotizacion(cotizar);
 };
 
-const obtenerCotizacion = async () => {
-  cargando.value = true
-  cotizacion.value = {}
 
-  try {
-    const { moneda, criptomoneda } = cotizar;
-    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
 
-    const respuesta = await fetch(url);
-    const data = await respuesta.json();
-    cotizacion.value = data.DISPLAY[criptomoneda][moneda]
-
-  } catch (error) {
-    console.log(error)  
-  } finally {
-    cargando.value = false
-  }
-
-};
-
-const mostrarResultado = computed(() => {
-  return Object.values(cotizacion.value).length > 0
-})
 
 </script>
 
