9fdfe69b8c3c35d23b5c54659ebeabafc377bfb5
CMV\lofd394905 <david.lopez@cajamorelia.com.mx>
2025-12-30 11:12:29 -0600

    163
    163

--- cripto-vue/src/App.vue
+++ cripto-vue/src/App.vue
@@ -3,8 +3,9 @@ import { ref, reactive } from "vue";
 import Alerta from "./components/Alerta.vue";
 import Spinner from "./components/Spinner.vue";
 import useCripto from "./composables/useCripto";
+import Cotizacion from "./components/Cotizacion.vue";
 
-const { monedas, criptomonedas, cargando, cotizacion, obtenerCotizacion, mostrarResultado } = useCripto() // composable
+const { monedas, criptomonedas, cotizacion, cargando, obtenerCotizacion, mostrarResultado } = useCripto() // composable
 
 
 const error = ref("");
@@ -69,23 +70,11 @@ const cotizarCripto = () => {
       </form>
 
       <Spinner v-if="cargando" />
+      <Cotizacion 
+        v-if="mostrarResultado"
+        :cotizacion="cotizacion"
+      />
 
-      <div v-if="mostrarResultado" class="contenedor-resultado">
-        <h2>Cotización</h2>
-        <div class="resultado">
-          <img 
-            :src="'https://cryptocompare.com/' + cotizacion.IMAGEURL" 
-            alt="imagen cripto"
-          >
-          <div>
-            <p>El precio es de: <span>{{ cotizacion.PRICE }}</span></p>
-            <p>Precio más alto del día: <span>{{ cotizacion.HIGHDAY }}</span></p>
-            <p>Precio más bajo del día: <span>{{ cotizacion.LOWDAY }}</span></p>
-            <p>Variación últimas 24 horas: <span>{{ cotizacion.CHANGEPCT24HOUR }}%</span></p>
-            <p>Última actualización: <span>{{ cotizacion.LASTUPDATE }}</span></p>
-          </div>
-        </div>
-      </div>  
     </div>
   </div>
 </template>
